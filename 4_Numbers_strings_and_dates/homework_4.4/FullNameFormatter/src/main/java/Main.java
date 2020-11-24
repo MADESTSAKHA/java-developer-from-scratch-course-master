14a05e725743351aead1836708b36d83f8e4d9f9
Sokolov Vladimir (MADESTSAKHA) <madestsakha@gmail.com>
2020-11-25 01:24:34 +0900

    4 Numbers, strings and dates. Homework # 4.4. Task#3 - FullNameFormatter

--- 4_Numbers_strings_and_dates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 4_Numbers_strings_and_dates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -1,19 +1,45 @@
+/*import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        }
+    }
+}*/
 import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+        String input = scanner.nextLine();
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      System.out.print("Введите ФИО: ");
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      System.out.println("Введенная строка не является ФИО");
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        int spaceIndex = input.indexOf(' ');
+        int spaceIndex2 = input.lastIndexOf(' ');
+
+        String surname = input.substring(0, spaceIndex).trim();
+        String name = input.substring(spaceIndex, spaceIndex2).trim();
+        String middleName = input.substring(spaceIndex2 + 1).trim();
+
+        if (input.equals("0")) {
+            System.out.println("Введенная строка не является ФИО");
+        }
+
+        //TODO:напишите ваш код тут, результат вывести в консоль.
+        //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+        System.out.println("Фамилия: " + surname);
+        System.out.println("Имя: " + name);
+        System.out.println("Отчество: " + middleName);
     }
-  }
 }
