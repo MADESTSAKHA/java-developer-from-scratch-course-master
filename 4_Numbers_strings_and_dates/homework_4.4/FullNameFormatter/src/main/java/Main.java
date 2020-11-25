3fbb18a12275194d8249baba2f3664f081f44d56
Sokolov Vladimir (MADESTSAKHA) <madestsakha@gmail.com>
2020-11-25 14:54:19 +0900

    4 Numbers, strings and dates. Homework # 4.4. Task#3 - FullNameFormatter

--- 4_Numbers_strings_and_dates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 4_Numbers_strings_and_dates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -1,7 +1,6 @@
-/*import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -10,36 +9,25 @@ public class Main {
             if (input.equals("0")) {
                 break;
             }
-            //TODO:напишите ваш код тут, результат вывести в консоль.
-            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+            for (int i = 0; i < input.length(); i++) {
+                int spaceIndex3 = input.length() - input.replaceAll(" ", "").length(); //Поиск Space(ов)
+                if (Character.isDigit(input.charAt(i)) || spaceIndex3 == 0 || spaceIndex3 == 3) {    //проверка на Space
+                    System.out.println("Введенная строка не является ФИО");
+                    break;
+                }
+                if (i % input.length() == 0) {
+                    int spaceIndex = input.indexOf(' ');
+                    int spaceIndex2 = input.lastIndexOf(' ');
+
+                    String surname = input.substring(0, spaceIndex).trim();
+                    String name = input.substring(spaceIndex, spaceIndex2).trim();
+                    String middleName = input.substring(spaceIndex2 + 1).trim();
+
+                    System.out.println("Фамилия: " + surname);
+                    System.out.println("Имя: " + name);
+                    System.out.println("Отчество: " + middleName);
+                }
+            }
         }
-    }
-}*/
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-//        while (true) {
-        String input = scanner.nextLine();
-
-        int spaceIndex = input.indexOf(' ');
-        int spaceIndex2 = input.lastIndexOf(' ');
-
-        String surname = input.substring(0, spaceIndex).trim();
-        String name = input.substring(spaceIndex, spaceIndex2).trim();
-        String middleName = input.substring(spaceIndex2 + 1).trim();
-
-        if (input.equals("0")) {
-            System.out.println("Введенная строка не является ФИО");
-        }
-
-        //TODO:напишите ваш код тут, результат вывести в консоль.
-        //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-
-        System.out.println("Фамилия: " + surname);
-        System.out.println("Имя: " + name);
-        System.out.println("Отчество: " + middleName);
     }
 }
