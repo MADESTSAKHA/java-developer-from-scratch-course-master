ef6bc41fca239cf978f4bb640636591ad43bdd10
Sokolov Vladimir (MADESTSAKHA) <madestsakha@gmail.com>
2020-11-26 17:57:13 +0900

    4 Numbers, strings and dates. Homework # 4.4. Task#4 - MaskingInformation - supplemented.

--- 4_Numbers_strings_and_dates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 4_Numbers_strings_and_dates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        text = text.replaceAll("(<).*?(>)", "***");
+        text = text.replaceAll("(\\(|\\[|\\{|\\<).*?(\\)|\\]|\\}|\\>)", "***");
         return text;
     }
 }
