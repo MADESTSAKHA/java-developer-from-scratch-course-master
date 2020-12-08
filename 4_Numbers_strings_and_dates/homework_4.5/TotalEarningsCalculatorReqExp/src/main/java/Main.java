d3b70a43b333c101e5e35b4ca41ac46a48af24f4
Sokolov Vladimir (MADESTSAKHA) <madestsakha@gmail.com>
2020-12-08 23:54:30 +0900

    4 Numbers, strings and dates. Homework # 4.5. Task#1 - TotalEarningsCalculatorReqExp

--- 4_Numbers_strings_and_dates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 4_Numbers_strings_and_dates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+    }
 
-  public static void main(String[] args) {
+    public static int calculateSalarySum(String text) {
+        text = text.replaceAll("[^0-9,\\,]", ""); //Выборка цифр и (запятых). Замена всего на "пустоту".
+        String[] sentences = text.split("\\,");             //Разделение строки по (запятой).
 
-  }
-
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
-
+        if (text.equals("")) {                                    //Проверка на пустоту.
+            return 0;
+        }
+        int sum = 0;
+        for (int i = 0; i < sentences.length; i++) {
+            sum += Integer.parseInt(sentences[i]);                 //Сложение цифр.
+        }
+        return sum;                                                //Вывод суммы сложения.
+    }
 }
