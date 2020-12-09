d8bd8d95081a32ef78d8bce93df85c7b452246f2
Sokolov Vladimir (MADESTSAKHA) <madestsakha@gmail.com>
2020-12-09 12:11:56 +0900

    4 Numbers, strings and dates. Homework # 4.5. Task#2 - SplitText

--- 4_Numbers_strings_and_dates/homework_4.5/SplitText/src/main/java/Main.java
+++ 4_Numbers_strings_and_dates/homework_4.5/SplitText/src/main/java/Main.java
@@ -1,12 +1,29 @@
 public class Main {
+    public static void main(String[] args) {
+    }
 
-  public static void main(String[] args) {
+    public static String splitTextInToWords(String text) {
+        //TODO реализуйте метод
 
-  }
+        text = text.replaceAll("[^a-zA-Z’]", " ");  //Вывод только букв, остальное меняем на "space"8o).
+        String[] sentences = text.split("\\s+");              //Разделяем текст на массив строк.
+        StringBuilder builder = new StringBuilder();                //Добавление строк друг за другом.
 
-  public static String splitTextInToWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
+        String words = text;
 
+        if (text.length() != 0) {                                   //Проверка на пустую строчку.
+            for (int i = 0; i < sentences.length; i++) {
+                builder.append(sentences[i]);                       //Вывод слова.
+                if (i < (sentences.length - 1)) {                   //Ограничитель переноса на новую строку.
+                    builder.append("\n");                           //Перенос слова на новую строку.
+                }
+                words = builder.toString();                         //На всякий случай .toString().
+            }
+        } else {
+            words = "";                                             //Вывод пустой строки.
+        }
+        return words;                                               //Вывод пустой или построчный.
+    }
 }
+
+// FIXME Кажется в этом задание тест сломан?
