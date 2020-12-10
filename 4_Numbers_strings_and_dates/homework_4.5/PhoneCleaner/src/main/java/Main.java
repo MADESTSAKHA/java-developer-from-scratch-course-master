6605ff41c0af03c80872d2b145cb7bda5cee0458
Sokolov Vladimir (MADESTSAKHA) <madestsakha@gmail.com>
2020-12-10 10:13:30 +0900

    4 Numbers, strings and dates. Homework # 4.5. Task#4 - PhoneCleaner

--- 4_Numbers_strings_and_dates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 4_Numbers_strings_and_dates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
 
-  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        String input = scanner.nextLine();
+        String text = input.replaceAll("[^0-9]", "");//Выбираем русс алфавит, ост меняем на space.
+
+        if (text.charAt(0) == '7' || text.charAt(0) == '8') {
+            if (text.length() < 10 || text.length() > 11) {
+                System.out.println("Неверный формат номера");
+            } else {
+                if (text.length() == 11 && text.charAt(0) == '8') {
+                    System.out.println(text.replace(text.charAt(0), '7'));
+                } else {
+                    System.out.println(text);
+                }
+            }
+        } else {
+            if (text.length() == 10 && text.charAt(0) != '7' && text.charAt(0) != '8') {
+                System.out.println("7" + text);
+            } else {
+                System.out.println("Неверный формат номера");
+            }
+        }
     }
-  }
-
 }
+             /**  Не очень сложно, но лучше садиться за такие задачки с утра, на свежую голову!  */
+
+//        System.out.println("Неверный формат номера");
+//        System.out.println(text);
