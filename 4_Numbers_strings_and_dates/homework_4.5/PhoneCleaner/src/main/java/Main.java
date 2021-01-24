5489640ff9f36900382132d7498c18647369d115
Sokolov Vladimir (MADESTSAKHA) <madestsakha@gmail.com>
2021-01-24 15:48:21 +0900

    Intermediate option

--- 4_Numbers_strings_and_dates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 4_Numbers_strings_and_dates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -8,26 +8,31 @@ public class Main {
         String input = scanner.nextLine();
         String text = input.replaceAll("[^0-9]", "");//Выбираем русс алфавит, ост меняем на space.
 
-        if (text.charAt(0) == '7' || text.charAt(0) == '8') {
-            if (text.length() < 10 || text.length() > 11) {
-                System.out.println("Неверный формат номера");
-            } else {
-                if (text.length() == 11 && text.charAt(0) == '8') {
-                    System.out.println(text.replace(text.charAt(0), '7'));
-                } else {
-                    System.out.println(text);
-                }
-            }
-        } else {
-            if (text.length() == 10 && text.charAt(0) != '7' && text.charAt(0) != '8') {
-                System.out.println("7" + text);
-            } else {
-                System.out.println("Неверный формат номера");
-            }
-        }
     }
 }
-             /**  Не очень сложно, но лучше садиться за такие задачки с утра, на свежую голову!  */
+
+/*    Scanner scanner = new Scanner(System.in);
+
+    String input = scanner.nextLine();
+    String text = input.replaceAll("[^0-9]", "");//Выбираем русс алфавит, ост меняем на space.
+
+        if (text.charAt(0) == '7' || text.charAt(0) == '8') {
+                if (text.length() < 10 || text.length() > 11) {
+        System.out.println("Неверный формат номера");
+        } else {
+        if (text.length() == 11 && text.charAt(0) == '8') {
+        System.out.println(text.replace(text.charAt(0), '7'));
+        } else {
+        System.out.println(text);
+        }
+        }
+        } else {
+        if (text.length() == 10 && text.charAt(0) != '7' && text.charAt(0) != '8') {
+        System.out.println("7" + text);
+        } else {
+        System.out.println("Неверный формат номера");
+        }
+        }*/
 
 //        System.out.println("Неверный формат номера");
 //        System.out.println(text);
